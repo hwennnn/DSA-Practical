15272c7a5ed072d893c419d9bf32db6a27cbf18f
hwen <whman63@gmail.com>
2020-12-02 13:51:53 +0800

    update q2

--- Week 7/q2.cpp
+++ Week 7/q2.cpp
@@ -5,9 +5,9 @@ using namespace std;
 // g++ --std=c++17 q2.cpp -o ./output.out && ./output.out
 
 void printBackward(int array[], int n){
-    if (n >= 0){
-        cout << array[n] << " ";
-        printBackward(array, n-1);
+    if (n > 0){
+        printBackward(array+1, n-1);
+        cout << array[0] << " ";
     }
 }
 
@@ -15,7 +15,7 @@ int main(){
     int arr[] = {1,2,3,4,5,6};
     int length = sizeof(arr) / sizeof(int);
 
-    printBackward(arr, length-1);
+    printBackward(arr, length);
     cout << endl;
 
     return 0;
