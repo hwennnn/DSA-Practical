e62090e120bf42205c119128a5fee874c625ae0f
hwen <whman63@gmail.com>
2020-12-02 14:06:07 +0800

    add comments for q3

--- Week 7/q3.cpp
+++ Week 7/q3.cpp
@@ -9,8 +9,11 @@ int maxArray(int array[], int start, int end){
     if (start == end){
         return array[start];
     }else{
-        int mid = (start + end) >> 1;
+        int mid = (start + end) >> 1; // to get the middle index of the array
         return max(maxArray(array, start, mid), maxArray(array, mid+1, end));
+        // we keep comparing the maximum of left side and right side ot the array 
+        // until it reached the base condition which they are narrowed down to left only one element,
+        // which is start == end
     }
 }
 
