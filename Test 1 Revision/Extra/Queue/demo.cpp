0cf954e26fbf5e574ab04c58f248529cd0fe3863
hwen <whman63@gmail.com>
2020-12-11 23:04:55 +0800

    push_front

---
+++ Test 1 Revision/Extra/Queue/demo.cpp
@@ -0,0 +1,25 @@
+#include "Queue.h"
+
+typedef int ItemType;
+
+using namespace std;
+
+// g++ --std=c++17 *.cpp -o ./output.out && ./output.out
+
+int main()
+{
+    Queue q;
+    int a = 1, b=2,c=3;
+    q.enqueue(a);
+    q.enqueue(b);
+    q.enqueue(c);
+
+    q.display();
+    q.push_front(69);
+    q.display();
+    q.dequeue();
+    q.display();
+
+    q.dequeue();
+    q.display();
+}
